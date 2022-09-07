a0b1ddda074d92d96552c95201e49547402b6c5d
Nikita Popkov <yongeryk@gmail.com>
2022-09-07 11:43:00 +0500

    dnd ветка сделана

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -21,7 +21,34 @@ const homeworkContainer = document.querySelector('#app');
 
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+function random(from, to) {
+  return Number.parseInt(from + (to - Math.random() * to));
+}
+
+export function createDiv() {
+  const minSize = 20,
+    maxSize = 200,
+    maxColor = 0xffffff;
+  const div = document.createElement('div');
+  div.draggable = true;
+  div.style.height = random(minSize, maxSize) + 'px';
+  div.style.width = random(minSize, maxSize) + 'px';
+  div.style.background = '#' + random(0, maxColor).toString(16);
+  div.style.top = random(0, window.innerHeight) + 'px';
+  div.style.left = random(0, window.innerWidth) + 'px';
+  div.classList.add('draggable-div');
+  div.addEventListener('dragstart', (e) => {
+    setTimeout(() => {
+      div.classList.add('hide');
+    }, 0);
+  });
+  div.addEventListener('dragend', (e) => {
+    div.classList.remove('hide');
+    div.style.top = `${e.clientY - Number.parseInt(div.style.height) / 2}px`;
+    div.style.left = `${e.clientX - Number.parseInt(div.style.width) / 2}px`;
+  });
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
